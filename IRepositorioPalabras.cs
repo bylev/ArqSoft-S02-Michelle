73c8aa8aa84d76491f64a2a49fe71a7f9c55a9de
bylev <michelle.camara@tecdesoftware.edu.mx>
2026-05-12 12:45:05 -0600

    refactor: separar en IRepositorioPalabras y PalabrasEnMemoria

---
+++ IRepositorioPalabras.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ahorcado
+{
+    internal interface IRepositorioPalabras
+    {
+        public interface IRepositorioPalabras
+        {
+            string ObtenerPalabraAleatoria();
+        }
+    }
+}
---
+++ PalabrasEnMemoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ahorcado
+{
+    internal class PalabrasEnMemoria : IRepositorioPalabras
+    {
+        private readonly List<String> _palabras = new()
+        {
+            "arquitectura", "interfaz", "polimorfismo",
+            "encapsulamiento", "herencia"
+        };
+
+        public string ObtenerPalabraAleatoria()
+        {
+            var random = new Random();
+            return _palabras[random.Next(_palabras.Count)];
+        }
+    }
+}
